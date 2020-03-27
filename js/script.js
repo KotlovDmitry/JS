8c8f77a81888d5fd93c023be2fccde32fd608a75
KotlovDmitry <kotlov.dmitry@gmail.com>
2020-03-27 16:51:31 +0300

    lesson2.1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-console.log('Hello')
+let money = 30000;
+let income = "Freelanse";
+let addExpenses = "Интернет, учеба, связь";
+let deposit = true;
+let mission = 1000000;
+let period = 12;
 
-var money;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
 
-alert ('Hello, World');
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase().split(" "));
+
+let budjetDay = money / 30
+console.log(budjetDay);
+
+
+
+
+
+
