fb6534edd40ed647abec931a3e8a62e9c25e01ea
KotlovDmitry <kotlov.dmitry@gmail.com>
2020-04-01 20:38:45 +0300

    добавили инпуты

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-let money = 30000;
-let income = "Freelanse";
-let addExpenses = "Интернет, учеба, связь";
-let deposit = true;
+'use strict'
+
+let money = prompt('Ваш месячный доход?');
+//let income = "Freelanse";
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 1000000;
 let period = 12;
+let expense1 = prompt('Введите обязательную статью расходов');
+let expense2 = prompt("Введите обязательную статью расходов");
+let amount1 = prompt('Во сколько это обойдется?');
+let amount2 = prompt("Во сколько это обойдется?");
+let budjetMonth = (money - amount2);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+
+//console.log(typeof money);
+//console.log(typeof income);
+//console.log(typeof deposit);
 console.log(addExpenses.length);
 
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
 console.log(addExpenses.toLowerCase().split(" "));
+console.log("Цели можно достичь примерно за " + (mission / budjetMonth) + " месяца(ев)");
 
-let budjetDay = money / 30
-console.log(budjetDay);
+
+let budjetDay = budjetMonth / 30
+console.log(budjetDay + "-ваш бюджет за день");
 
 
 
